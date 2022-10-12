239ff93326ccf1aea3192912acb1fca9c5fad62f
santiabe04 <santiagoabelle04@gmail.com>
2022-10-12 19:25:03 -0300

    First Commit

---
+++ alu/header.h
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/un.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <thread>
+#include <stdio.h>
+#include <iostream>
+#include <sys/sendfile.h>
+#include <vector>
+#include <semaphore.h>
+#include <mutex>
+#include <sys/wait.h>
+#include <cassert>
+#define PORT 5030
+#define MENSAJE_MAXIMO  256
+#define MAX_CLIENTS 10
+#define CLIENT_READY "CL_OK\n"
+#define CANT_CLIENTS 9
+
+using namespace std;
+
+// Ejemplo sencillo. 
+// Recordar que es posible definir clases
+// También se pueden definir funciones dentro del struct
+
+struct request{
+	char type[10];
+	char msg[MENSAJE_MAXIMO];
+};
+
+// OPCIONAL
+struct client_request{
+	// TO DO
+};
+
+// Funciones utiles sugeridas
+int read_sock(char str[], int s);
+void broadcast(vector<int>& sockets, struct request* req);
+void get_request(struct request* req, int s);
+void send_request(struct request* req, int s)
+int set_acc_socket(int lsn_port);
+void accept_conns(int s, vector<int>& v );
+
---
+++ alu/utils.cpp
@@ -0,0 +1,60 @@
+#include "header.h"
+
+// Un regalito, puede que quieran modificarla
+// Dado un arreglo de char str y un socket descriptor s, hace una lectura
+// bloqueante sobre s y llena el buffer str con los bytes leídos.
+int read_sock(char str[], int s) 
+{
+    int n;
+    n = recv(s, str, 2*MENSAJE_MAXIMO, 0);
+
+    if (n == 0) 
+        return -1;
+    if (n < 0) { 
+        perror("recibiendo");
+        exit(1);
+    }
+    str[n] = '\0';
+    printf("%d\n",n);
+    printf("recibi: %s\n",str);
+    return 0;
+}
+
+
+// Dado un puntero a un request req y un socket s, recibe una request en s y la 
+// almacena en req. La funcion es bloqueante
+void get_request(struct request* req, int s)
+{
+   // TO DO  
+}
+
+// Dado un vector de enteros que representan socket descriptors y un request,
+// envía a traves de todos los sockets la request.
+void broadcast(vector<int>& sockets, struct request* req)
+{
+    // TO DO    
+}
+
+// Por siempre, acepta conexiones sobre un socket s en estado listen y 
+// agrega los sockets asociados al vector v.
+void accept_conns(int s, vector<int>& v)
+{
+     // TO DO  
+}
+// Dado un puerto lsn_port devuelve un socket en estado listen asociado
+// a todas las interfaces de red local y a ese puerto (ej 127.0.0.1:lsn_port)
+int set_acc_socket(int lsn_port)
+{
+     // TO DO  
+}
+
+// Setea un socket al modo nobloqueante
+static int nonblockingsocket(int s)
+{
+    int flags;
+
+    if ((flags = fcntl(s, F_GETFL)) == -1)
+        return -1;
+
+    return fcntl(s, F_SETFL, flags | O_NONBLOCK);
+}
