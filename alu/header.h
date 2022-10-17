8feca6080fe7aa796a9e71cf863b6e79da293a57
atienzag04 <gatienza@alumno.huergo.edu.ar>
2022-10-16 21:01:54 -0400

    Agregue funciones

--- alu/header.h
+++ alu/header.h
@@ -49,7 +49,7 @@ struct client_request{
 int read_sock(char str[], int s);
 void broadcast(vector<int>& sockets, struct request* req);
 void get_request(struct request* req, int s);
-void send_request(struct request* req, int s)
+void send_request(struct request* req, int s);
 int set_acc_socket(int lsn_port);
 void accept_conns(int s, vector<int>& v );
 
